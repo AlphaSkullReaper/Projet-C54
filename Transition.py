9fe3fa7fc610016c2ba201896c49bf2810e877a7
Russell Jimmies <88112401+RussellJimmies@users.noreply.github.com>
2022-10-24 13:12:26 -0400

    Classe Transition

--- Transition.py
+++ Transition.py
@@ -1,4 +1,28 @@
-# Boogy File, does nothing, Help Will
+from abc import ABC, abstractmethod
+from typing import Union 
 
-class Transition:
-    help = "Help"
+class Transition(ABC):
+    def __init__(self, next_state:State = None):
+        self.__next_state = next_state
+
+    @property
+    def is_valid(self):
+        return self.next_state is not None
+
+    @property
+    def next_state(self):
+        return self.__next_state
+
+    @next_state.setter
+    def next_state(self, new_state):
+        self.__next_state = new_state
+
+    @abstractmethod
+    def is_transiting(self):
+        pass
+
+    def _do_transiting_action(self):
+        pass
+    
+    def _exec_transiting_action(self):
+        self._do_transiting_action()
